e8bb50689ed391817543c4379231e0b49df7d0a5
SandraMorales09 <114303948+SandraMorales09@users.noreply.github.com>
2024-12-25 22:44:19 -0600

    Add files via upload

---
+++ Javascript exercism/hola-mundo.js
@@ -0,0 +1,11 @@
+//
+// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+// strings in 25 "" or ''
+
+export function hello() {
+    return 'Hello, World!';
+  }
+  
